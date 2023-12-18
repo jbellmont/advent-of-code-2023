bfa0ad7ee5445d6431f7b56bfda877dc82c49070
Jack Bellmont <bellmont@gmail.com>
2023-12-18 18:41:39 +0000

    Complete day-5 part-1 - absolute bastard!

--- day-4/part-1/index.ts
+++ day-4/part-1/index.ts
@@ -1,19 +1,23 @@
 import { readFile } from "node:fs/promises";
 
 const formatAsArrayOfNumbers = (numbersList: string): number[] => {
-  return numbersList.split(/\D/).filter(value => value !== '').map(value => Number(value));
-}
+  return numbersList
+    .split(/\D/)
+    .filter((value) => value !== "")
+    .map((value) => Number(value));
+};
 
 const getSumOfScratchcardPoints = async (
-    pathToScratchcards: string
-  ): Promise<number | undefined> => {
-    try {
-      const scratchcards = (await readFile(pathToScratchcards, { encoding: "utf-8" })).split(
-        "\n"
-      );
+  pathToScratchcards: string,
+): Promise<number | undefined> => {
+  try {
+    const scratchcards = (
+      await readFile(pathToScratchcards, { encoding: "utf-8" })
+    ).split("\n");
 
-      const sumOfScratchcardPoints = scratchcards.reduce((totalGamePoints, currentGame) => {
-        const numbers = currentGame.split(':')[1].split('|');
+    const sumOfScratchcardPoints = scratchcards.reduce(
+      (totalGamePoints, currentGame) => {
+        const numbers = currentGame.split(":")[1].split("|");
 
         const winningNumbers = formatAsArrayOfNumbers(numbers[0]);
         const winnersMap = new Map<number, boolean>();
@@ -22,26 +26,30 @@ const getSumOfScratchcardPoints = async (
         }
 
         const numbersYouHave = formatAsArrayOfNumbers(numbers[1]);
-        const pointsForCurrentGame = numbersYouHave.reduce((currentGamePoints, currentNumber) => {
-          if (winnersMap.has(currentNumber)) {
-            if (currentGamePoints === 0) {
-              return 1;
+        const pointsForCurrentGame = numbersYouHave.reduce(
+          (currentGamePoints, currentNumber) => {
+            if (winnersMap.has(currentNumber)) {
+              if (currentGamePoints === 0) {
+                return 1;
+              }
+
+              return currentGamePoints * 2;
             }
 
-            return currentGamePoints * 2;
-          }
-
-          return currentGamePoints;
-        }, 0);
+            return currentGamePoints;
+          },
+          0,
+        );
 
         return totalGamePoints + pointsForCurrentGame;
-      }, 0);
-  
-      return sumOfScratchcardPoints;
-    } catch (error) {
-      console.error("Had trouble reading file provided!", error);
-    }
-  };
-  
-  console.log(await getSumOfScratchcardPoints("day-4/part-1/input.txt"));
-  
+      },
+      0,
+    );
+
+    return sumOfScratchcardPoints;
+  } catch (error) {
+    console.error("Had trouble reading file provided!", error);
+  }
+};
+
+console.log(await getSumOfScratchcardPoints("day-4/part-1/input.txt"));
--- day-4/part-2/index.ts
+++ day-4/part-2/index.ts
@@ -1,11 +1,14 @@
 import { readFile } from "node:fs/promises";
 
 const formatAsArrayOfNumbers = (numbersList: string): number[] => {
-  return numbersList.split(/\D/).filter(value => value !== '').map(value => Number(value));
-}
+  return numbersList
+    .split(/\D/)
+    .filter((value) => value !== "")
+    .map((value) => Number(value));
+};
 
 const getCardCountForSingleGame = (scratchcard: string): number => {
-  const numbers = scratchcard.split(':')[1].split('|');
+  const numbers = scratchcard.split(":")[1].split("|");
 
   const winningNumbers = formatAsArrayOfNumbers(numbers[0]);
   const winnersMap = new Map<number, boolean>();
@@ -27,32 +30,43 @@ const getCardCountForSingleGame = (scratchcard: string): number => {
   }, 0);
 
   return matches;
-}
+};
 
-const getCardCountOfRange = (scratchcards: string[], startIndex: number, length: number): number => {
+const getCardCountOfRange = (
+  scratchcards: string[],
+  startIndex: number,
+  length: number,
+): number => {
   let cards = 0;
 
   for (let i = startIndex; i < length; i++) {
     const cardsForSingleGame = getCardCountForSingleGame(scratchcards[i]);
-    const cardsFromRangeOfCards = getCardCountOfRange(scratchcards, i + 1, i + (cardsForSingleGame + 1));
-    cards += (cardsForSingleGame + cardsFromRangeOfCards);
+    const cardsFromRangeOfCards = getCardCountOfRange(
+      scratchcards,
+      i + 1,
+      i + (cardsForSingleGame + 1),
+    );
+    cards += cardsForSingleGame + cardsFromRangeOfCards;
   }
 
   return cards;
-}
+};
 
 const getSumOfScratchcards = async (
-    pathToScratchcards: string
-  ): Promise<number | undefined> => {
-    try {
-      const scratchcards = (await readFile(pathToScratchcards, { encoding: "utf-8" })).split(
-        "\n"
-      );
+  pathToScratchcards: string,
+): Promise<number | undefined> => {
+  try {
+    const scratchcards = (
+      await readFile(pathToScratchcards, { encoding: "utf-8" })
+    ).split("\n");
 
-      return getCardCountOfRange(scratchcards, 0, scratchcards.length) + scratchcards.length;
-    } catch (error) {
-      console.error("Had trouble reading file provided!", error);
-    }
-  };
-  
-  console.log(await getSumOfScratchcards("day-4/part-2/input.txt"));
+    return (
+      getCardCountOfRange(scratchcards, 0, scratchcards.length) +
+      scratchcards.length
+    );
+  } catch (error) {
+    console.error("Had trouble reading file provided!", error);
+  }
+};
+
+console.log(await getSumOfScratchcards("day-4/part-2/input.txt"));
